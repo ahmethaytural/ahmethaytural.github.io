b3818976bb87df3f65cd5e8d8bf2933cb313afbf
Ahmet Haytural <45502781+ahmethaytural@users.noreply.github.com>
2021-04-28 04:54:11 +0300

    design update

---
+++ assets/js/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	$(document).on('click','.play',function(){
+
+		var watch = $(this).attr('data-watch'),
+			scope = $(this).parents('.intro');
+
+		$(this).remove();
+		$(scope).append('<div class="loading" data-watch="nvWsuI5PKZY"></div>');
+		window.location.replace("https://youtu.be/"+watch);
+
+	});
+
+});
