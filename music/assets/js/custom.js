3e59370d48fd6056406f8fccb21cc3f14603d9c3
Ahmet Haytural <45502781+ahmethaytural@users.noreply.github.com>
2022-01-30 07:44:37 +0300

    js for youtube embed code

--- music/assets/js/custom.js
+++ music/assets/js/custom.js
@@ -1,16 +1,21 @@
 $(document).ready(function(){
 
 	$(document).on('click','.play',function(){
-
+/*
 		var watch = $(this).attr('data-watch'),
 			scope = $(this).parents('.intro');
 
 		$(this).remove();
 		$(scope).append('<div class="loading" data-watch="nvWsuI5PKZY"></div>');
-		window.location.replace("https://youtu.be/"+watch);
+		window.location.replace("https://youtu.be/"+watch);*/
+
+		var watch = $(this).attr('data-watch'),
+			scope = $(this).parents('.intro');
+
+			$(this).parent('.intro').html('<iframe width="450" height="253" src="https://www.youtube.com/embed/'+watch+'" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
 
 	});
 
 	$('.link').attr('target','_blank');
-	
+
 });
